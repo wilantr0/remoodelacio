d775b31ff651964395aac01fea1cfcd198370ca6
Guillermo <guilleballarin73@gmail.com>
2024-10-08 17:20:11 +0200

    ahora no me da ningun error 🤞

--- src/app/api/ping/route.ts
+++ src/app/api/ping/route.ts
@@ -1,7 +1,8 @@
 import { conn } from '@/lib/database';
 
 export async function GET() {
-  const query = await conn.query('SELECT NOW()')
+  const query = conn ? await conn.query('SELECT NOW()') : {rows:"Error"}
+
 
   return Response.json(query.rows)
 }
