056f7d5affc88176907b32d0b9a589eca04c115d
Lieety <lieety10.07@gmail.com>
2024-10-28 09:36:45 +0100

    commit changes

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -6,7 +6,6 @@ export default function Error () {
     <body>
       <div className="flex justify-center items-center h-screen bg-black">
         <img src="https://http.cat/404" alt="errorCat" />
-        
       </div>
     </body>
     </html>
