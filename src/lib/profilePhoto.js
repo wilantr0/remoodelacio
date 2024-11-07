eff6cf0a182d3c0f8816d853d48979cd95146703
Guillermo <guilleballarin73@gmail.com>
2024-11-07 18:01:45 +0100

    redeploy

--- src/lib/profilePhoto.js
+++ src/lib/profilePhoto.js
@@ -1,35 +1,24 @@
-import { createCanvas } from 'canvas';
+import { createCanvas } from '@napi-rs/canvas';
 
 export default function generateProfilePicture(name, size = 100) {
-  // Crear un canvas
   const canvas = createCanvas(size, size);
   const ctx = canvas.getContext('2d');
 
-  // Dibujar un círculo
   ctx.beginPath();
   ctx.arc(size / 2, size / 2, size / 2 - 5, 0, 2 * Math.PI);
   const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  ctx.fillStyle = randomColor; // Color de fondo del círculo
+  ctx.fillStyle = randomColor;
   ctx.fill();
 
-  // Obtener las iniciales de cada palabra
   const initials = name
     .split(' ')
     .map(word => word.charAt(0).toUpperCase())
     .join('');
-
-  // Configurar el estilo de texto y dibujar las iniciales en el centro
   ctx.font = `bold ${size * 0.4}px sans-serif`;
   ctx.textAlign = 'center';
   ctx.textBaseline = 'middle';
-  ctx.fillStyle = '#333'; // Color del texto
+  ctx.fillStyle = '#333';
   ctx.fillText(initials, size / 2, size / 2);
 
-  // Convertir el canvas a una imagen base64
-  const dataURL = canvas.toDataURL();
-
-  return dataURL;
+  return canvas.toDataURL();
 }
-
-// Ejemplo de uso
-console.log(generateProfilePicture('Manolo'));
